bbf7b20729bab92d698529d18a66b3e70d043345
Duplo-P <petroithnmanuel@gmail.com>
2025-02-14 13:15:05 +0100

    widgets

--- src/gui.py
+++ src/gui.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-
+from tkinter import messagebox
 
 class JanelaPrincipal:
     def __init__(self, master):
@@ -7,15 +7,29 @@ class JanelaPrincipal:
         master.title("Duplo File Master Alfa 0.1")
         master.resizable(False, False)
         self.centralizar_janela()
-
+        comprimento = 18
+        background_btb = "#000000"
+        font_color = "#FFFFFF"
+        font_size = ("Helvetica", 14)
+        
         # Criação dos widgets da interface
-        label = tk.Label(master, text="Exemplo de label")
-        label.pack()
+        frm = tk.Frame(self.master)
+        
+        btb_organizar = tk.Button(frm, width = comprimento, background = background_btb, foreground = font_color, font = font_size, text="Organizar")
+        btb_organizar.pack(pady = 20)
+        
+        btb_ajuda = tk.Button(frm, width = comprimento, background = background_btb, foreground = font_color, font = font_size, text="Ajuda")
+        btb_ajuda.pack(pady = 20)
+        
+        btb_sair = tk.Button(frm, width = comprimento, background = background_btb, foreground = font_color, font = font_size, text="Sair", command = self.sair)
+        btb_sair.pack(pady = 20)
+        
+        frm.pack(pady = 80)
 
-        botao = tk.Button(master, text="Calcular")
-        botao.pack()
-        self.centralizar_janela()
-
+    def sair(self):
+        message = messagebox.askquestion("Sair", "Deseja Sair?")
+        if message == "yes":
+            self.master.quit()
 
     def centralizar_janela(self):
         largura_janela = 600
