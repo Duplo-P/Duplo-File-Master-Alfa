0d5c8b60ba6524def06de937f18ef6c2a57ca6f1
Duplo-P <petroithnmanuel@gmail.com>
2025-02-15 12:30:55 +0100

    front e back terminado

--- src/funcionalidades.py
+++ src/funcionalidades.py
@@ -1,38 +1,22 @@
 import os
-from tkinter import filedialog, messagebox, Tk
 
 def info_arquivo():
     """
     Função para gerar informações das redes socias em arquivo txt.
     
     """
-    string ="""
-    Siga-nos nas redes sociais:
+    string ="""     Duplo File Master Alfa 0.1
+Siga-nos nas redes sociais:
     Instagram: @2p_analytcs
     Facebook: Duplo P Analytics
     """
-    caminho = get_caminho()
-    os.chdir(caminho)
-    try:
-        if not os.path.exists("info.txt"):
-            with open("info.txt", "w") as arquivo:
-                arquivo.write(string)
-    except:
-        messagebox.showerror("Erro", "Siga-nos nas redes sociais")
-        
-  
-def get_caminho():
-    """
-    Função para pegar o caminho da pasta a ser organizada.
-    
-    """
-    root = Tk()
-    root.withdraw()
+    #os.chdir(caminho)
 
-    caminho = filedialog.askdirectory()
-    root.destroy()
-    
-    return caminho
+    if not os.path.exists("info.txt"):
+        with open("info.txt", "w") as arquivo:
+            arquivo.write(string)
+
+        
 
 def moverArq(arquivo):
     """Essa função verifica a extensão do arquivo e move para a pasta correspondente.
@@ -53,25 +37,50 @@ def moverArq(arquivo):
     
     # Criação de dicionários para armazenar as extensão
     arq = {"Audios": extensao_audio, "Videos": extensao_video, "Imagens": extensao_imagem, "Documentos": extensao_doc, "Compactados": extensao_compactado, "Programas": extensao_exe, "Script": extensao_script}
-    
-    try:
+        
         # Verifica a extensão do arquivo e move para a pasta correspondente
         #percorrendo o dicionário
-        for chave, valor in arq.items():
-            #verificando se a extensão do arquivo está no valor(valor é uma tupla que contém as extensões)
+    extensao = os.path.splitext(arquivo)[1]
+    for chave, valor in arq.items():
+        #verificando se a extensão do arquivo está no valor(valor é uma tupla que contém as extensões)
+        
+        if extensao in valor:
+            #verificando se a pasta existe
+            if not os.path.exists(chave):
+                #Se não existir, cria a pasta
+                os.mkdir(chave)
+            #Move o arquivo para a pasta correspondente
+            os.rename(arquivo, chave + '/' + arquivo)
+            return
+    #Se a extensão não estiver na lista de extensões
+    if not os.path.exists("Outros Arquivos"):
+        os.mkdir("Outros Arquivos")
+    os.rename(arquivo, "Outros Arquivos/" + arquivo)
+
+        
+def moverDoc():
+    """Essa função verifica a extensão do arquivo e move para a pasta correspondente,
+    neste caso dentro da pasta Documentos, cria subpastas com base nas extensões. 
+    """
+    NomePasta = "Documentos"
+    extensao_doc = ('.txt', '.doc', '.docx', '.odt', '.pdf', '.rtf', '.tex', '.wpd', '.wps', '.xml', '.msg', '.odt', '.odp', '.ods', '.odg', '.odf', '.sxw', '.sxi', '.sxc', '.sxd', '.sxi', '.sxm', '.sxg', '.stw', '.stc', '.std', '.stc', '.stw', '.sxw', '.sxd', '.sxi', '.sxm', '.sxg', '.sxc', '.sxd')
+
+    os.chdir(NomePasta)
+    lista_doc = os.listdir(os.getcwd())
+    
+    for arquivo in lista_doc:
+        if os.path.isfile(arquivo):
+            extensao = os.path.splitext(arquivo)[1]
             
-            #if extensao in valor:
-            if arquivo.endswith(valor):
-                #verificando se a pasta existe
-                if not os.path.exists(chave):
-                    #Se não existir, cria a pasta
-                    os.makedirs(chave)
-                #Move o arquivo para a pasta correspondente
-                os.rename(arquivo, chave + '/' + arquivo)
-                break
-    except:
-        messagebox.showerror("Erro", "Erro ao mover arquivo ")   
-             
+            if extensao in extensao_doc:
+                if not os.path.exists(extensao):
+                    os.makedirs(extensao)
+                os.rename(arquivo, os.path.join(extensao, arquivo))
+            else:
+                if not os.path.exists("Arquivos Outros"):
+                    os.makedirs("Arquivos Outros")
+                os.rename(arquivo, os.path.join("Arquivos Outros", arquivo))
+
 def moverDoc():
     """Essa função verifica a extensão do arquivo e move para a pasta correspondente,
     neste caso dentro da pasta Documentos, cria subpastas com base nas extensões. 
@@ -79,78 +88,65 @@ def moverDoc():
 
     NomePasta="Documentos"
     extensao_doc = ('.txt', '.doc', '.docx', '.odt', '.pdf', '.rtf', '.tex', '.wpd', '.wps', '.xml', '.msg', '.odt', '.odp', '.ods', '.odg', '.odf', '.sxw', '.sxi', '.sxc', '.sxd', '.sxi', '.sxm', '.sxg', '.stw', '.stc', '.std', '.stc', '.stw', '.sxw', '.sxd', '.sxi', '.sxm', '.sxg', '.sxc', '.sxd')
-
-    try:
         
-        # Indica a pasta como a de trabalho(onde vai criar as subpastas)
-        os.chdir(NomePasta)
-        
-        # Lista de arquivos na pasta
-        lista_doc = os.listdir(os.getcwd()) 
-        
-        # Percorrendo a lista de arquivos
-        for arquivo in lista_doc:
-            
-            # Verifica se o arquivo é um arquivo
-            if os.path.isfile(arquivo):
-                
-                # Separa a extensão do arquivo
-                extensao = os.path.splitext(arquivo)[1]
-                
-                # Verifica a extensão do arquivo e move para a pasta correspondente
-                if extensao in extensao_doc:
-                    
-                    # Verifica se a pasta da extensão existe
-                    if not os.path.exists(extensao):
-                        
-                        # Se não existir, cria a pasta
-                        os.makedirs(extensao)
-                        
-                    # Move o arquivo para a pasta correspondente
-                    os.rename(os.getcwd() + "/" + arquivo, os.getcwd() + "/" + extensao + "/" + arquivo)
-                
-                # Se a extensão não estiver na lista de extensões de documentos
-                else:
-                    # Cria a pasta Outros, para armazenar os arquivos que as extensões não se encontram.
-                    if not os.path.exists("Arquivos Outros"):
-                        
-                        os.makedirs("Arquivos Outros")
-                    os.rename(os.getcwd() + "/" + arquivo, os.getcwd() + "/Arquivos Outros/" + arquivo)
+    # Indica a pasta como a de trabalho(onde vai criar as subpastas)
+    os.chdir(NomePasta)
     
-    except:
-        messagebox.showerror("Erro", "Erro ao mover arquivo")
+    # Lista de arquivos na pasta
+    lista_doc = os.listdir(os.getcwd()) 
+    
+    # Percorrendo a lista de arquivos
+    for arquivo in lista_doc:
+        
+        # Verifica se o arquivo é um arquivo
+        if os.path.isfile(arquivo):
+            
+            # Separa a extensão do arquivo
+            extensao = os.path.splitext(arquivo)[1]
+            
+            # Verifica a extensão do arquivo e move para a pasta correspondente
+            if extensao in extensao_doc:
+                
+                # Verifica se a pasta da extensão existe
+                if not os.path.exists(extensao):
+                    
+                    # Se não existir, cria a pasta
+                    os.mkdir(extensao)
+                    
+                # Move o arquivo para a pasta correspondente
+                os.rename(os.getcwd() + "/" + arquivo, os.getcwd() + "/" + extensao + "/" + arquivo)
+            
+            # Se a extensão não estiver na lista de extensões de documentos
+            else:
+                # Cria a pasta Outros, para armazenar os arquivos que as extensões não se encontram.
+                if not os.path.exists("Arquivos Outros"):
+                    
+                    os.mkdir("Arquivos Outros")
+                os.rename(os.getcwd() + "/" + arquivo, os.getcwd() + "/Arquivos Outros/" + arquivo)
 
-def organizar():
+def organizar(caminho):
     """
     Função para organizar os dados do arquivo de entrada.
     
    """
-    try:
-        # Caminho da pasta a organizar
-        caminho = get_caminho()
         
-        # Usar o caminho como pasta de trabalho
-        os.chdir(caminho)
+     # Usar o caminho como pasta de trabalho
+    os.chdir(caminho)
+    
+    # Lista de arquivos na pasta
+    lista_arquivos = os.listdir()
+    
+    #Percorendo a lista de arquivos
+    for arquivo in lista_arquivos:
         
-        # Lista de arquivos na pasta
-        lista_arquivos = os.listdir()
-        
-        #Percorendo a lista de arquivos
-        for arquivo in lista_arquivos:
+        # Verifica se o arquivo é um arquivo
+        if os.path.isfile(arquivo):
             
-            # Verifica se o arquivo é um arquivo
-            if os.path.isfile(arquivo):
-                
-                # Verifica a extensão do arquivo e move para a pasta correspondente
-                moverArq(arquivo)
-        
-        # Organiza os documentos
-        #moverDoc("Documentos")
-        
-        # Gera informações das redes sociais em arquivo txt
-        info_arquivo()
-        
-        messagebox.showinfo("Concluído", "Organização concluída com sucesso")
-        
-    except:
-        messagebox.showwarning("Aviso", "Nenhuma pasta selecionada")
+            # Verifica a extensão do arquivo e move para a pasta correspondente
+            moverArq(arquivo)
+    
+    # Organiza os documentos
+    #moverDoc("Documentos")
+    
+    # Gera informações das redes sociais em arquivo txt
+    info_arquivo()
--- src/gui.py
+++ src/gui.py
@@ -1,6 +1,7 @@
 import tkinter as tk
-from tkinter import messagebox
-from funcionalidades import organizar
+from tkinter import messagebox, filedialog
+#from src.funcionalidades import organizar, info_arquivo
+from funcionalidades import organizar, info_arquivo
 
 class JanelaPrincipal:
     def __init__(self, master):
@@ -18,25 +19,40 @@ class JanelaPrincipal:
         # Criação dos widgets da interface
         frm = tk.Frame(self.master, bg = "#201f1f")
         
-        btb_organizar = tk.Button(frm, width = comprimento, background = background_btb, foreground = font_color, font = font_size, text="Organizar", command = organizar)
+        btb_organizar = tk.Button(frm, width = comprimento, background = background_btb, foreground = font_color, font = font_size, text="Organizar", command = self.organizar)
         btb_organizar.config(cursor = "hand2")
         btb_organizar.pack(pady = 20)
 
         
         btb_ajuda = tk.Button(frm, width = comprimento, background = background_btb, foreground = font_color, font = font_size, text="Ajuda", command = self.ajuda)
         btb_ajuda.pack(pady = 20)
-        btb_organizar.config(cursor = "hand2")
+        btb_ajuda.config(cursor = "hand2")
 
         
         btb_sair = tk.Button(frm, width = comprimento, background = background_btb, foreground = font_color, font = font_size, text="Sair", command = self.sair)
         btb_sair.pack(pady = 20)
-        btb_organizar.config(cursor = "hand2")
+        btb_sair.config(cursor = "hand2")
 
         frm.pack( pady = 80)
         
         lbl_versao = tk.Label(self.master, text = "Duplo P Analytics - Versão 0.1   ", font = font_size_verson, bg = "#201f1f", fg = "#ffffff")
         lbl_versao.pack(side = "bottom", anchor="se")
-
+    def info(self):
+        try:
+            if self.caminho:
+                info_arquivo(self.caminho) 
+        except Exception as e:
+            messagebox.showerror(f"Erro", "Erro ao gerar informações do arquivo {e}")
+        
+    def organizar(self):
+        self.caminho = filedialog.askdirectory(parent=self.master, title="Selecione a pasta para organizar")
+        try:
+            if self.caminho:
+                organizar(self.caminho) 
+                messagebox.showinfo("Sucesso", "Arquivos organizados com sucesso!")
+        except Exception as e:
+            messagebox.showerror(f"Erro", "Erro ao organizar arquivos {e}")
+             
     def sair(self):
         message = messagebox.askquestion("Sair", "Deseja Sair?")
         if message == "yes":
